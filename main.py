f83e81bdbbfe0b74bebce16462f13f6581a8cec5
tekal119 <boero119@gmail.com>
2025-03-14 19:12:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -37,3 +37,4 @@ def actualizar_tareas():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Usar el puerto que Render asigna
     app.run(host="0.0.0.0", port=port)
+print(f"Servidor corriendo en http://0.0.0.0:{port}")
